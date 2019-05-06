f59251042911de9412fe3e7be080e6c6599a8e29
pinbraerts <shirshov.dima@gmail.com>
2019-05-06 03:48:46 +0300

    Add shaders

---
+++ shaders/base_vertex.hlsl
@@ -0,0 +1,33 @@
+cbuffer ModelViewProjectionConstantBuffer : register(b0)
+{
+	matrix model;
+	matrix view;
+	matrix projection;
+};
+
+struct VS_INPUT {
+	float3 position: POSITION;
+	float3 color: COLOR;
+};
+
+struct VS_OUTPUT {
+	float4 position: SV_POSITION;
+	float4 color: COLOR;
+};
+
+VS_OUTPUT main(VS_INPUT input) {
+	VS_OUTPUT output;
+
+	float4 pos = float4(input.position, 1.0f);
+
+	// Transform the position from object space to homogeneous projection space
+	pos = mul(pos, model);
+	pos = mul(pos, view);
+	pos = mul(pos, projection);
+	output.position = pos;
+
+	// Just pass through the color data
+	output.color = float4(input.color, 1.0f);
+
+	return output;
+}
