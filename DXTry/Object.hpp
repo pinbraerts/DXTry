3d837443c63af5b5e9834379fa4c9ca687ab4991
pinbraerts <shirshov.dima@gmail.com>
2019-05-09 12:38:14 +0300

    New Object

--- DXTry/Object.hpp
+++ DXTry/Object.hpp
@@ -3,68 +3,27 @@
 
 #include "includes.hpp"
 #include "Error.hpp"
+#include "Mesh.hpp"
+#include "Material.hpp"
 
-struct MaterialSerial {
-	std::wstring_view vertex_path;
-	std::wstring_view pixel_path;
-	std::vector<D3D11_INPUT_ELEMENT_DESC> descriptors;
-
-	struct Constant {
-		Vector3 ambient;
-		Vector3 diffuse;
-		Vector3 specular;
-		Vector3 shininess;
-	} constant;
-
-	std::wstring_view geometry_path;
-};
-
-struct ObjectSerial {
-	MaterialSerial material;
-	std::vector<float> vertices;
-	std::vector<USHORT> indices;
-
+struct ObjectData {
+	std::shared_ptr<Mesh> mesh;
+	std::shared_ptr<Material> material;
 	Matrix model;
-	UINT stride;
-	UINT offset = 0;
 };
 
-struct Material {
-	ComPtr<ID3D11VertexShader> vertex_shader;
-	ComPtr<ID3D11GeometryShader> geometry_shader;
-	//ComPtr<ID3D11Buffer> stream_output;
-	ComPtr<ID3D11PixelShader> pixel_shader;
-	ComPtr<ID3D11InputLayout> input_layout;
-
-	MaterialSerial::Constant material;
-
-	Material() = default;
-	Material(Engine& engine, const MaterialSerial& serial);
-
-	void init(Engine& engine, const MaterialSerial& serial);
-};
-
-struct Object: Material {
-	// ComPtr
-	ComPtr<ID3D11Buffer> vertex_buffer;
-	ComPtr<ID3D11Buffer> index_buffer;
-	ComPtr<ID3D11Buffer> constant_buffers[2];
-
-	// Constants
-	Matrix model;
-
-	// Other stuff
-	UINT n_indices;
-	UINT stride;
-	UINT offset;
+struct Object: IObject, ObjectData {
+	ComPtr<ID3D11Buffer> constant_buffer;
 
 	Object() = default;
-	Object(Engine& engine, const ObjectSerial& serial);
+	Object(ObjectData&& data);
+	Object(Engine& engine, ObjectData&& data);
 
-	void init(Engine& engine, const ObjectSerial& serial);
-	void update(Engine& engine);
-	void render(Engine& engine);
-	// void destroy(Engine& engine);
+	void set(ObjectData&& data);
+
+	void init(Engine& engine) override;
+	void render(Engine& engine) override;
+	void update(Engine& engine) override;
 };
 
 #endif // !DXTRY_OBJECT_HPP
